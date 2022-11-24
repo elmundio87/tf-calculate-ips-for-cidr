6d4fa5a6330ed02ac484f4441c5793c734f316e5
Edmund Dipple <elmundio1987@gmail.com>
2022-11-24 16:59:27 +0000

    Now supports up to 8 on bitmask

--- main.tf
+++ main.tf
@@ -1,17 +1,16 @@
 locals {
-  # bits = {
-  #   "18" = [4,4,4,4]
-  #   "19" = [3,3,3,3]
-  #   "20" = [2,2,2]
-  #   "21" = [3,3,3,3,3,3,3,3]
-  #   "22" = [2,2,2,2]
-  #   "23" = [1,1]
-  # }
 
-  cidr = "10.0.0.0/12"
-  divider = 22
+  cidr = "10.0.0.0/7"
   bitmask = split("/", local.cidr)[1]
-  split = local.bitmask >= local.divider ? [] : [ for x in range(pow(2, (local.divider - local.bitmask))) : local.divider - local.bitmask]
+
+  bitmask_minimum = 22
+  divider = local.bitmask < 10 ? 8 * (10 - local.bitmask) : 4
+  parts = concat([ for x in range(local.divider) : x/local.divider], [1])
+
+  split = local.bitmask >= local.bitmask_minimum ? [] : flatten([ for div in range(1,local.divider + 1) : [
+    for x in range(pow(2, (local.bitmask_minimum - local.bitmask)) * local.parts[div], pow(2, (local.bitmask_minimum - local.bitmask)) * local.parts[div - 1]) : local.bitmask_minimum - local.bitmask
+    ]
+  ])
 
   cidrs = length(local.split)  == 0 ? [local.cidr] : cidrsubnets(local.cidr, local.split...)
 
@@ -22,13 +21,25 @@ locals {
 
 }
 
+# output "parts" {
+#   value = local.parts
+# }
+
+output "cidr" {
+  value = local.cidr
+}
+
 output "ip_addresses" {
   value = length(local.ip_addresses)
 }
 
-output "test" {
-  value = pow(2, (local.divider - local.bitmask))
-}
+# output "cidrs" {
+#   value = length(local.cidrs)
+# }
+
+# output "test" {
+#   value = pow(2, (local.bitmask_minimum - local.bitmask))
+# }
 
 # output "bitmask" {
 #   value = local.bitmask
@@ -38,8 +49,6 @@ output "test" {
 #   value = local.split
 # }
 
-output "cidrs" {
-  value = length(local.cidrs)
-}
+
 
 
